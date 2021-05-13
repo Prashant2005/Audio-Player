af09f427b6236bf6448f14cd28c77e9f099b3a0b
Prashant2005 <83994388+Prashant2005@users.noreply.github.com>
2021-05-13 15:59:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ let curr_track = document.createElement('audio');
 // Define the tracks that have to be played
 let track_list = [
   {
-    name: "Night Owl",
-    artist: "Broke For Free",
-    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+    name: "Khali Thi wo Zindagi",
+    artist: "Pyaar Lafzon Mein Kahan",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7TSHJl0IKRoGfIln28iDwGQ6XJ8qdT0BmuA&usqp=CAU",
+    path: "Download.mp3"
   },
   {
     name: "Enthusiast",
